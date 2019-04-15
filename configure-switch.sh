743950d5cdc6ab4d82183d64d6b57a0208aeff0c
Erik Moldtmann <erik@moldtmann.de>
2019-04-15 21:25:05 +0200

    prepare for HB appstore

--- configure-switch.sh
+++ configure-switch.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 pushd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=../NintendoSwitchToolchain.cmake -DCMAKE_INSTALL_PREFIX=output -DSWITCH=true
+cmake .. -DCMAKE_TOOLCHAIN_FILE=../NintendoSwitchToolchain.cmake -DCMAKE_INSTALL_PREFIX=blobby -DSWITCH=true
 popd
